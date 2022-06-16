e2806a26159ac6f0ce12d4574f9caae14a2a4039
Mazhe <mazhe@wasu.com>
2022-06-16 11:10:31 +0800

    ReverseLinkedList

---
+++ 0.Algorithm/1.ReverseLinkedList/LinkedList.go
@@ -0,0 +1,61 @@
+package main
+
+import "fmt"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func main() {
+
+	head := new(ListNode)
+	head.Val = 0
+
+	node1 := new(ListNode)
+	node1.Val = 1
+
+	node2 := new(ListNode)
+	node2.Val = 2
+
+	node3 := new(ListNode)
+	node3.Val = 3
+
+	node4 := new(ListNode)
+	node4.Val = 4
+
+	node5 := new(ListNode)
+	node5.Val = 5
+
+	head.Next = node1
+	node1.Next = node2
+	node2.Next = node3
+	node3.Next = node4
+	node4.Next = node5
+
+	Println(ReverseList(head))
+}
+
+func ReverseList(node *ListNode) *ListNode {
+	if node == nil {
+		return nil
+	}
+
+	var pre *ListNode
+
+	for node != nil {
+		temp := node.Next
+		node.Next = pre
+		pre = node
+		node = temp
+	}
+
+	return pre
+}
+
+func Println(list *ListNode) {
+	for list != nil {
+		fmt.Printf("val:%v ", list.Val)
+		list = list.Next
+	}
+}
