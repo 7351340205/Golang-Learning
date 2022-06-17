4534b76febfd2dd15ea3677efdfcf4496b3df074
Mazhe <mazhe@wasu.com>
2022-06-17 10:46:22 +0800

    HelloWorld

---
+++ 2.Fundamentals in Golang/1.Structure of Golang/1.Hello World.go
@@ -0,0 +1,27 @@
+//定义一个包的名字
+package main
+
+//导入外部包，调用外部包中的函数
+//若 import . "fmt"
+//则在下方代码中调用其函数无需加包名
+//Println("xxx")
+
+//若 import _ "fmt"
+//则无需使用包也可以编译执行
+import "fmt"
+
+//func 为声明函数关键字
+//每一个包仅能有一个main函数、一个init函数
+//加载顺序为 导入的包init->导入的包main->本包init->本包main
+
+//OutPut:
+//Package main inited
+//Hello World!
+
+func main() {
+	fmt.Println("Hello World!")
+}
+
+func init() {
+	fmt.Println("Package main inited")
+}
